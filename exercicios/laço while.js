1062d4ce2052d2d0aa2441e77795d70f2c7bd7f6
Camila Ramos Medeiros <milanina.cr@gmail.com>
2025-09-13 20:01:29 -0300

    Add files via upload

---
+++ exercicios/laço while.js
@@ -0,0 +1,12 @@
+let i = 10; 
+
+while (i >= 1) { 
+  console.log(i);
+  i--; 
+}
+
+// Inicio a variável i com 10.
+
+//O while continua rodando enquanto a condição for verdadeira.
+
+//A cada passo, imprimimos i e diminuímos 1 com i--.
