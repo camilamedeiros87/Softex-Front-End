7105ba315c509bfcd7bdb369a79986f1d2659b1c
Camila Ramos Medeiros <milanina.cr@gmail.com>
2025-09-13 19:46:52 -0300

    Add files via upload

---
+++ exercicios/laço for básico.js
@@ -0,0 +1,11 @@
+for (let i = 1; i <= 10; i++) {
+  console.log(i);
+}
+
+//let i = 1; será o nosso contatdor
+
+//i <= 10; é a condição de continuação do loop. Enquanto i for menor ou igual a 10, o código dentro do loop será executado.
+
+//i++ Após cada execução do loop, o valor de i é aumentado em 1. Assim, o loop irá avançar do 1 para o 2, do 2 para o 3 e assim por diante.
+
+//console.log(i); Esta linha imprime o valor atual de i no console em cada iteração do loop.
