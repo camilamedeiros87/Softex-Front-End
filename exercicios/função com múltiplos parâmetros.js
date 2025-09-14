40dd556d3cd854148a8e94f8c10b030ccbf2400e
Camila Ramos Medeiros <milanina.cr@gmail.com>
2025-09-14 11:05:11 -0300

    Add files via upload

---
+++ exercicios/função com múltiplos parâmetros.js
@@ -0,0 +1,7 @@
+function apresentar(nome, idade, cidade) {
+  return `Meu nome é ${nome}, tenho ${idade} anos e moro em ${cidade}.`;
+}
+
+// Exemplos de uso:
+console.log(apresentar("Camila", 37, "Olinda"));
+console.log(apresentar("Sérgio", 30, "Rio de Janeiro"));
